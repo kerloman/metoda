0e638247d416fc08e40e1799b515ac747d9d2bf4
Kirill <kirillkonakov@gmail.com>
2025-04-15 22:03:15 +0300

    Initial Commit

---
+++ MetodaTests/MetodaTests.swift
@@ -0,0 +1,16 @@
+//
+//  MetodaTests.swift
+//  MetodaTests
+//
+//  Created by Кирилл Конаков on 15.04.2025.
+//
+
+import Testing
+
+struct MetodaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
